b83c8461e5050721497b6ab536c0f375b4f9a9c7
Arina <arina2004popova@yandex.ru>
2021-12-08 15:38:48 +0300

    Изменены названия переменных

--- main.py
+++ main.py
@@ -13,14 +13,13 @@ env = Environment(
 template = env.get_template('template.html')
 date = int(os.path.split(os.path.split(str(datetime.datetime.now().date()).replace('-', '/'))[0])[0])
 
-excel_data_df = pandas.read_excel('wine3.xlsx',
+drinks = pandas.read_excel('wine3.xlsx',
                                   sheet_name='Лист1',
                                   usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
-                                  na_values=['N/A', 'NA'], keep_default_na=False)
-table = excel_data_df.to_dict(orient='records')
+                                  na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='records')
 category_products = defaultdict(list)
 
-for drink in table:
+for drink in drinks:
     category_products[drink["Категория"]].append(drink)
     rendered_page = template.render(
         date=date - 1920,
